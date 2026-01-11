f40a0ad827f712f35b7ad6ec45094f8ae4238d4e
daguillon23 <146889679+daguillon23@users.noreply.github.com>
2026-01-10 22:10:30 -0600

    Update download.py

--- cik_module/download.py
+++ cik_module/download.py
@@ -1,24 +1,31 @@
 import boto3
 import requests
+import json
 
-BUCKET = "aguillon-tickers"
+# replace with your bucket name
+BUCKET = None
 KEY = 'company_tickers_exchange.json'
 
 def lambda_handler(event, context):
-    url = event['url']
-    HEADERS = event['headers']
-    response = requests.get(url, headers=HEADERS)
-    s3 = boto3.client('s3')
+    try:
 
-    # creates bucket
-    #bucket = s3.create_bucket(Bucket=BUCKET, CreateBucketConfiguration={"LocationConstraint": "us-east-2"})
-    
-    s3.put_object(Bucket=BUCKET, Key=KEY, Body=response.content)
+        url = "https://www.sec.gov/files/company_tickers_exchange.json"
+        HEADERS = {"user-agent": "MLT DA diego.eaguillon@gmail.com"}
+        response = requests.get(url, headers=HEADERS)
+        s3 = boto3.client('s3')
 
-    # prints body of key in bucket
-    #print(s3.get_object(Bucket=BUCKET, Key=KEY)['Body'].read())
+        # creates bucket
+        #bucket = s3.create_bucket(Bucket=BUCKET, CreateBucketConfiguration={"LocationConstraint": "us-east-2"})
+        
+        s3.put_object(Bucket=BUCKET, Key=KEY, Body=response.content)
 
-    return {
-        'statusCode': 200,
-        'body': "EDGAR UPLOAD SUCCESS"
-    }
+        # prints body of key in bucket
+        # print(s3.get_object(Bucket=BUCKET, Key=KEY))
+        # print(s3.get_object(Bucket=BUCKET, Key=KEY)['Body'][0])
+        print("EDGAR UPLOAD SUCCESS")
+        return {
+            'statusCode': 200,
+            'body': "EDGAR UPLOAD SUCCESS"
+        }
+    except Exception as e:
+        print(f"ERROR: {e}")
